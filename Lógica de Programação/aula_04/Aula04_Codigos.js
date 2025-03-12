0d73a8111b77c9f76afb489844b47aeab11eff5f
raphael <raphaelferraz5056@gmail.com>
2025-03-12 17:03:28 -0300

    Adição de arquivos Lógica de programação

---
+++ Lógica de Programação/aula_04/Aula04_Codigos.js
@@ -0,0 +1,4 @@
+// Inicializa um array a
+//     1 ;      2       
+a = []
+for(i = 0; i <a.length; a[i++] = 0) ;
---
+++ Lógica de Programação/aula_04/Aula04_Exercicio08.js
@@ -0,0 +1,43 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
+console.log(prod1)
+const prod2 = {}
+prod2.nome = 'Celular Ultra Mega'
+prod2.preco = 4998.90
+prod2['Desconto Legal'] = 0.40 // evitar atributos com espaço
+
+var vet = [1,2,3,4,5, "Michel"]
+console.log(vet)
+//const prod2 = {
+//    nome: 'Camisa Polo',
+//    preco: 79.90
+//}
+//console.log(prod2)
+
+//const pessoa = {
+//    nome : 'Michel',
+//    idade : 32,
+//    altura : 1.82
+//}
+//console.log(pessoa)
+
+//var s = "hello"; // Começa com um texto em letras minúsculas
+//console.log(s)
+//console.log(s.toUpperCase()) // Retorna "HELLO", mas não altera s
+//console.log(s)
+//s // => "hello": a string original não mudou
+
+var a = 0
+var b = 0
+var a, b
+var nome = ""
+var valor = 0
+console.log(a == b)
+
+
+
+
+
+
