87d1e0fafbef82b37d0562a56850f9dd000a5399
raphael <raphaelferraz5056@gmail.com>
2025-03-13 17:14:09 -0300

    Adição de muita coisa kkkk

---
+++ Lógica de Programação/teste.js
@@ -0,0 +1,21 @@
+/*
+var pessoa = {
+    nome: "Raphael",
+    sobrenome: "Ferraz"
+};
+
+pessoa["Raphael"] = "Pedro"
+pessoa.sobrenome = "Ribeiro"
+
+saida = pessoa["Raphael"];
+console.log(saida);
+console.log(pessoa.sobrenome)
+
+var antes = new Date(2024, 11, 3);
+
+console.log(antes.getMonth());
+console.log(antes.getDay());
+*/
+conversao = parseInt("05061498119")
+
+console.log(conversao)
